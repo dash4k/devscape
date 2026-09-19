9b8698429f472574fbe46411123fbd113b25a410
Danishwara Pracheta <danishwara81@gmail.com>
2026-09-19 17:25:17 +0800

    feat: added References page

---
+++ src/pages/ReferencesPage.jsx
@@ -0,0 +1,11 @@
+import DrugList from '../components/DrugList.jsx';
+
+const ReferencesPage = () => {
+  return (
+    <div className="w-full h-auto px-5 pt-3">
+      <DrugList />
+    </div>
+  );
+};
+
+export default ReferencesPage;
