9f86e5a0ccdcfbe1ea3b443a28a62c5e645a2f78
Danishwara Pracheta <danishwara81@gmail.com>
2026-09-19 17:19:56 +0800

    feat: added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="w-full pb-4 pt-8 mt-10 bg-surface border-t border-border-strong">
+      <div className="flex flex-col gap-lg justify-between max-w-250 mx-auto px-container-margin">
+        <h2 className="text-headline-lg font-display font-bold text-text-primary">
+            Devscape
+        </h2>
+        <p className="font-body-sm text-body-sm text-text-muted">
+                    &#xA9; 2026 Ni Nyoman Devina Arista
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
