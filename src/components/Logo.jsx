cfd2e88fa6e2593337a78d3299f26f4b31595f18
Danishwara Pracheta <danishwara81@gmail.com>
2026-09-20 17:37:42 +0800

    chore: updated text size

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <p className="flex items-center gap-0 px-3 text-text-primary text-display cursor-pointer">
+    <p className="flex items-center gap-0 px-3 text-text-primary text-display text-2xl cursor-pointer">
       <span className="text-accent-primary">Dev</span>scape
     </p>
   );
